e51efe1471a8581bc1c31418069c487674828a73
Tobias Andersson <tobiaserikandersson@gmail.com>
2025-02-23 20:40:38 +0200

    small changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,33 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage';
 import HomePage from './pages/HomePage/HomePage';
 import ChatPage from './pages/ChatPage/ChatPage';
+import Navbar from './components/Navigation/Navigation';
 import './App.css'
 
-function App() {
+function AppContent() {
+  const location = useLocation(); 
+  const showNavbar = location.pathname !== '/'; 
 
   return (
-      <Router>
-        <div className='flex flex-col justify-center items-center h-screen w-full'>
-          <Routes>
-            <Route path='/' element={<LoginPage/>}/>
-            <Route path='/home' element={<HomePage/>}/>
-            <Route path='/chat/:chatId/messages' element={<ChatPage />} />
-          </Routes>
-        </div>
-      </Router>
-  )
+    <div className="flex flex-col justify-center items-center h-screen w-full">
+      {showNavbar && <Navbar />} 
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/chat/:chatId/messages" element={<ChatPage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+}
+
+export default App;
